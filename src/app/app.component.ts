0368e1194b02396e8dee8c3ca6559e7b1d2d7299
Kathiresan Sankaran <kathir1007@gmail.com>
2021-04-30 17:37:43 +0530

    initial commt with components basic with single component

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.test.html',
+  styleUrls: ['./app.test.css']
+})
+
+export class AppComponent {
+  title = 'basic-operations';
+}
